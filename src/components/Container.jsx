55c9a4346a39ecfd72d87964260da268387390f5
Akshita Sharma <69713523+akshita1812-alt@users.noreply.github.com>
2023-12-31 18:44:47 +0530

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,14 +2,14 @@ import style from "./Container.module.css";
 import PropTypes from 'prop-types';
 
 function Container({handleClick, advice}) {
-    console.log(advice)
+    // console.log(advice)
 
     return (
       <div className={style.container}>
         <div className={style.mainContainer}>
           <p>Advice #{advice.slip.id}</p>
           <h1 className={style.quotes}>
-            "{advice.slip.advice}"     </h1>
+            {advice.slip.advice}   </h1>
           <span className={style.border}></span>
         </div>
         <button className={style.dice} onClick={handleClick}>
